8b5340af5ca918d118e70ec87e839263c3b7d29a
Chad Homan <choman@gmail.com>
2023-12-06 17:51:37 -0600

    shellcheck

--- .bashrc.d/30.util.nix
+++ .bashrc.d/30.util.nix
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 NIX_PROFILE="${HOME}/.nix-profile/etc/profile.d/nix.sh"
 NIX_HOME_MANAGER="${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
 source_if_exists "${NIX_PROFILE}"
