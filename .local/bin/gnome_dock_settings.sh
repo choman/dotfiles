3ec86fe1b65f5ee19ca2cd2519ed45e2c066b07a
Chad Homan <choman@gmail.com>
2025-02-07 10:52:52 -0600

    initial

--- .local/bin/gnome_dock_settings.sh
+++ .local/bin/gnome_dock_settings.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 #
 #
+#
+#
+DOCK_FIXED=false
+DOCK_AUTOHIDE=true
+DOCK_INTELLIHIDE=true
+DOCK_ICON_SIZE=38
+UBIN=/usr/bin
 
+echo "Detecting physical/virtual system - via systemd-detect-virt"
 if systemd-detect-virt -q; then
-   echo "Physical"
+   echo "  - virtual"
+   DOCK_AUTOHIDE=false
+   DOCK_INTELLIHIDE=false
 fi
+
+${UBIN}/gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed $DOCK_FIXED
+${UBIN}/gsettings set org.gnome.shell.extensions.dash-to-dock audtohide $DOCK_AUTOHIDE
+## ${UBIN}/gsettings set org.gnome.shell.extensions.dash-to-dock intellihide $DOCK_INTELLIHIDE
+${UBIN}/gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size $DOCK_ICON_SIZE
