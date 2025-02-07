3755c27ca5034bb4dee4e3caf1c512b670cda2b0
Chad Homan <choman@gmail.com>
2025-02-07 10:58:47 -0600

    initial

--- .local/bin/gnome_clock_settings.sh
+++ .local/bin/gnome_clock_settings.sh
@@ -2,6 +2,7 @@
 
 UBIN=/usr/bin
 
+echo "Setting gnome clock settings"
 ${UBIN}/gsettings set org.gnome.desktop.interface clock-format '12h'
 ${UBIN}/gsettings set org.gnome.desktop.interface clock-show-weekday true
 ${UBIN}/gsettings set org.gnome.desktop.calendar show-weekdate true
