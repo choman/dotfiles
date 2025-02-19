9838cdce33c9573cffeab831be445418e3021eb8
Chad Homan <choman@gmail.com>
2025-02-19 01:02:02 -0600

    shfmt

--- .config/bashrc.d/30.util.nix
+++ .config/bashrc.d/30.util.nix
@@ -4,4 +4,3 @@ NIX_PROFILE="${HOME}/.nix-profile/etc/profile.d/nix.sh"
 NIX_HOME_MANAGER="${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
 source_if_exists "${NIX_PROFILE}"
 source_if_exists "${NIX_HOME_MANAGER}"
-
